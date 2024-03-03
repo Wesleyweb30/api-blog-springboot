1bdf3e60703506091d07a55235089976380bcc74
wesleyWeb <wesleyweb30@gmail.com>
2024-03-03 08:24:11 -0300

    Create User domain

---
+++ blog/src/main/java/com/wesleyluan/blog/domain/User.java
@@ -0,0 +1,27 @@
+package com.wesleyluan.blog.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String name;
+    @Column(unique = true,nullable = false)
+    private String email;
+    private String password;
+}
