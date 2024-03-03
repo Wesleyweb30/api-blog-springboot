9ad28db31d1f4e1daddfd1543d8e680276042a6a
wesleyWeb <wesleyweb30@gmail.com>
2024-03-03 09:05:54 -0300

    Create User Controller with findAll and save

---
+++ blog/src/main/java/com/wesleyluan/blog/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.wesleyluan.blog.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.wesleyluan.blog.domain.User;
+import com.wesleyluan.blog.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @PostMapping
+    public User save(@RequestBody User obj) {
+        return  userService.save(obj);
+    }
+    
+    
+}
